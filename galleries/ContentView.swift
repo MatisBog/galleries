40f70b8b293df319d75454e820435e4e51f3549f
Matis Bogaert <Matis.Bogaert@iMacD.local>
2025-12-11 11:13:59 +0100

    init

---
+++ galleries/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  galleries
+//
+//  Created by Matis Bogaert on 11/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ galleries/galleriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  galleriesApp.swift
+//  galleries
+//
+//  Created by Matis Bogaert on 11/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct galleriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
